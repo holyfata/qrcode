92ce966df6e0124a2780e8db697141ad5bf40a59
HolyFata <holyfata@outlook.com>
2025-04-12 23:04:22 +0800

    Initial commit

---
+++ apps/app-vue/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { HfQrcode } from "package-vue"
+</script>
+
+<template>
+	<div>
+		<HfQrcode />
+	</div>
+</template>
---
+++ packages/package-vanilla/stencil.config.ts
@@ -0,0 +1,31 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { vueOutputTarget, ComponentModelConfig } from '@stencil/vue-output-target';
+
+
+export const config: Config = {
+  namespace: 'package-vanilla',
+  outputTargets: [
+    reactOutputTarget({
+      // Relative path to where the React components will be generated
+      outDir: '../package-react/src/stencil-output',
+    }),
+    vueOutputTarget({
+      componentCorePackage: 'package-vanilla',
+      includeDefineCustomElements: true,
+      proxiesFile: '../package-vue/src/stencil-output/components.ts'
+    }),
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+  ],
+  testing: {
+    browserHeadless: "shell",
+  },
+};
